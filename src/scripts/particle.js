a829797bfbc12d57e17ac7e1bfdb6391e51bbb04
Anna <viirall@yandex.ru>
2020-04-24 15:38:20 +0300

    up

--- src/scripts/particle.js
+++ src/scripts/particle.js
@@ -37,50 +37,50 @@ if (navigator.userAgent.indexOf('Chrome')!=-1){
   isApple = false;
 }
 
-if(valentines && !(Safari || InternetExplorer)) {
-  const getWidth = document.getElementsByClassName('section__main')[0].offsetWidth;
-  const getHeight = document.getElementsByClassName('section__main')[0].offsetHeight;
-  let heart = new Particle({
-    renderer: isApple? 'canvas' : 'webgl',
-    image: document.querySelector('#valentines'),
-    width: isMobile ? getWidth : window.innerWidth,
-    height: isMobile ? getWidth < 800 ? 370 : getHeight / 1.5 : window.innerHeight,
-    particleGap: settings.particleGap,
-    particleSize: settings.particleSize,
-    mouseForce: settings.mouseForce,
-    noise: settings.noise,
-    layerCount: settings.layerCount,
-    layerDistance: settings.layerDistance
-  });
-
-
-  function redraw(getWidth, getHeight) {
-    heart.particleGap = settings.particleGap;
-    heart.particleSize = settings.particleSize;
-    heart.mouseForce = settings.mouseForce;
-    heart.noise = settings.noise;
-    heart.layerCount = settings.layerCount;
-    heart.layerDistance = settings.layerDistance;
-    heart.width = isMobile ? getWidth : window.innerWidth;
-    heart.height = isMobile ? getWidth < 800 ? 370 : getHeight / 1.5 : window.innerHeight;
-
-    heart.start({
-      initPosition: 'none',
-      initDirection: 'none'
+if(valentines) {
+  if(!(Safari || InternetExplorer)) {
+    const getWidth = document.getElementsByClassName('section__main')[0].offsetWidth;
+    const getHeight = document.getElementsByClassName('section__main')[0].offsetHeight;
+    let heart = new Particle({
+      renderer: isApple ? 'canvas' : 'webgl',
+      image: document.querySelector('#valentines'),
+      width: isMobile ? getWidth : window.innerWidth,
+      height: isMobile ? getWidth < 800 ? 370 : getHeight / 1.5 : window.innerHeight,
+      particleGap: settings.particleGap,
+      particleSize: settings.particleSize,
+      mouseForce: settings.mouseForce,
+      noise: settings.noise,
+      layerCount: settings.layerCount,
+      layerDistance: settings.layerDistance
     });
+    function redraw(getWidth, getHeight) {
+      heart.particleGap = settings.particleGap;
+      heart.particleSize = settings.particleSize;
+      heart.mouseForce = settings.mouseForce;
+      heart.noise = settings.noise;
+      heart.layerCount = settings.layerCount;
+      heart.layerDistance = settings.layerDistance;
+      heart.width = isMobile ? getWidth : window.innerWidth;
+      heart.height = isMobile ? getWidth < 800 ? 370 : getHeight / 1.5 : window.innerHeight;
 
-  };
+      heart.start({
+        initPosition: 'none',
+        initDirection: 'none'
+      });
 
-  window.addEventListener('resize', function () {
-    const getWidth = document.getElementsByClassName('section__main')[0].offsetWidth;
-    const getHeight = document.getElementsByClassName('section__main')[0].offsetHeight;
-    redraw(getWidth, getHeight);
-  });
-  window.addEventListener('orientationchange', function () {
-    const getWidth = document.getElementsByClassName('section__main')[0].offsetWidth;
-    const getHeight = document.getElementsByClassName('section__main')[0].offsetHeight;
-    redraw(getWidth, getHeight);
-  });
-} else {
-  valentines.setAttribute("src", "assets/images/erga.jpg");
+    };
+
+    window.addEventListener('resize', function () {
+      const getWidth = document.getElementsByClassName('section__main')[0].offsetWidth;
+      const getHeight = document.getElementsByClassName('section__main')[0].offsetHeight;
+      redraw(getWidth, getHeight);
+    });
+    window.addEventListener('orientationchange', function () {
+      const getWidth = document.getElementsByClassName('section__main')[0].offsetWidth;
+      const getHeight = document.getElementsByClassName('section__main')[0].offsetHeight;
+      redraw(getWidth, getHeight);
+    });
+  } else {
+    valentines.setAttribute("src", "assets/images/erga.jpg");
+  }
 }
